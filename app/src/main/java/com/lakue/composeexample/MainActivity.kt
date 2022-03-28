083759555e9535438b50448436d6d5fc53b4a7fa
ChoiMyungRak <audfkr0808@naver.com>
2022-03-28 20:50:04 +0900

    compose sample

--- app/src/main/java/com/lakue/composeexample/MainActivity.kt
+++ app/src/main/java/com/lakue/composeexample/MainActivity.kt
@@ -3,15 +3,26 @@ package com.lakue.composeexample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TextCreate()
+            NewStory()
         }
     }
 
@@ -19,5 +30,46 @@ class MainActivity : AppCompatActivity() {
     @Composable
     private fun TextCreate() {
         Text(text = "helloWorld")
+    }
+
+    @Preview
+    @Composable
+    fun NewStory() {
+
+        MaterialTheme {
+            val typography = MaterialTheme.typography
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_background),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .height(180.dp)
+                        .fillMaxWidth()
+                        .clip(shape = RoundedCornerShape(4.dp)),
+                    contentScale = ContentScale.Crop
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Text(
+                    text = "A day wandering through the sandhills " +
+                            "in Shark Fin Cove, and a few of the " +
+                            "sights I saw",
+                    style = typography.h6,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    text = "Davenport, California",
+                    style = typography.body2
+                )
+                Text(
+                    text = "December 2018",
+                    style = typography.body2
+                )
+            }
+        }
+
     }
 }
